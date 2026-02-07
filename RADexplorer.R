b741c3f30eaa28312531d8f8ede2e526fe64c0c0
jakob young <jakyoung@byu.edu>
2026-02-07 14:04:26 -0700

    Add RADexplorer app

---
+++ RADexplorer.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(bslib)
+library(plotly)
+source("~/Desktop/RAD/visualization.R")
+
+ui = page_sidebar(
+  title = "RAD Explorer",
+  sidebar = sidebar(
+    title = "Navigate",
+    textInput(
+      "taxon",
+      label = "Input taxon to explore:",
+      value = NULL
+    ),
+    checkboxGroupInput(
+      "varRegions",
+      "Select all 16S gene variable regions to include:",
+      choices = list("1" = 1,"2" = 2,"3" = 3,"4" = 4,"5" = 5,"6" = 6,"7" = 7,"8" = 8,"9" = 9),
+      selected = c(3,4)
+    ),
+    
+    actionButton("submit", "Submit")
+  ),
+  card(plotlyOutput("visual", height = "650px"))
+)
+
+server = function(input, output, session) {
+  
+  msa_plot <- eventReactive(input$submit, {
+    make_msa_plotly(
+      taxon = input$taxon,
+      varRegions = input$varRegions
+    )
+  })
+  
+  output$visual <- renderPlotly({
+    msa_plot()
+  })
+}
+
+
+
+shinyApp(ui = ui, server = server)
+
