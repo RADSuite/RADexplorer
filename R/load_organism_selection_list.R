cb7b149642882bcd97881542706d52eb5fd4f33c
jakob young <jakyoung@byu.edu>
2026-03-31 17:26:59 -0600

    added the number of species to the '[Genus] - All Species' options in the taxaSelect dropdown

--- R/load_organism_selection_list.R
+++ R/load_organism_selection_list.R
@@ -1,6 +1,6 @@
 #' Load list used in RADexplorer dropdown
 #'
-#' @param accessions_table an table with columns "organism" and "genus"
+#' @param accessions_table a table with columns "organism" and "genus"
 #'
 #' @returns
 #' @export
@@ -12,16 +12,18 @@ load_organism_selection_list <- function(accessions_table){
   selection_list <- vector("character", list_size)
   curr_index <- 1
   for (curr_genus in genera){
-    selection_list[curr_index] <- paste0(curr_genus, " - All Species")
-    curr_index <- curr_index + 1 
     unordered_species <- accessions_table |>
       (function(x) x[accessions_table$genus == curr_genus, ])() |>
       (function(x) x$organism)() |>
       unique()
+
+    selection_list[curr_index] <- paste0(curr_genus, " - All Species (", length(unordered_species), ")")
+    curr_index <- curr_index + 1
+
     ordered_species <- unordered_species[order(!grepl("^[a-zA-Z]", unordered_species), unordered_species)]
     for(species in ordered_species){
       selection_list[curr_index] <- species
-      curr_index <- curr_index + 1 
+      curr_index <- curr_index + 1
     }
   }
   return(selection_list)
